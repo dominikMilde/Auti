8c2e134730598548a74d33eed72dfad930a8a487
Dominik Milde <dominik.milde@gmail.com>
2017-02-09 11:20:19 +0100

    dodali smo brands php

--- connect.php
+++ connect.php
@@ -9,7 +9,6 @@ if ($mysqli->connect_errno) {
     echo "Error: Failed to make a MySQL connection, here is why: \n";
     echo "Errno: " . $mysqli->connect_errno . "\n";
     echo "Error: " . $mysqli->connect_error . "\n";
-
     exit;
 }
 ?>
