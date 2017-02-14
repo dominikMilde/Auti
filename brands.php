1bf716754c141d35fad6de5212f097e256429392
Dominik Milde <dominik.milde@gmail.com>
2017-02-14 18:43:12 +0100

    a

--- brands.php
+++ brands.php
@@ -11,7 +11,7 @@
         <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-              <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
+              <a href="index.html" class="site_title"><i class="fa fa-car"></i> <span> Automobili!</span></a>
             </div>
 
             <div class="clearfix"></div>
@@ -94,7 +94,27 @@
                 </div>
               </div>
             </div>
-          </div>
+          </div><div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>Glavni izbornik</h3>
+                <ul class="nav side-menu">
+                  <li><a><i class="fa fa-home"></i> Početna <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="index.html">Dashboard</a></li>
+                      <li><a href="index2.html">Dashboard2</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-edit"></i> Forme <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="form.html">General Form</a></li>
+                      <li><a href="form_advanced.html">Advanced Components</a></li>
+                      <li><a href="form_validation.html">Form Validation</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+
+            </div>
         </div>
         <!-- /page content -->
 
