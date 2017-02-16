d36f6f7b509115f84785cb2b168aea89acf608b0
Dominik Milde <dominik.milde@gmail.com>
2017-02-16 10:55:52 +0100

    sql query za ispis brandova

--- brands_create.php
+++ brands_create.php
@@ -1,5 +1,10 @@
 <?php
 
-    require("connect.php");
-    
+require('connect.php');
+
+$sql = "INSERT INTO brands (name, year_est) VALUES ('" . $_POST["name"] . "', '" . $_POST["year_est"] . "');" ;
+$mysqli->query($sql);
+
+headers('Location: brands.php');
+
 ?>
