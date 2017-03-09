71232ed1523805a373e4b14b1e6b334a722aa237
Dominik Milde <dominik.milde@gmail.com>
2017-03-09 10:57:06 +0100

    a

--- cars_update.php
+++ cars_update.php
@@ -4,6 +4,7 @@ require('connect.php');
 
 $sql = "UPDATE cars SET brand_id='" .$_POST['brand_id']. "', model='" . $_POST['model'] . "', color='" . $_POST['color'] . "', mileage='" . $_POST['mileage'] . "' WHERE id=" . $_POST['id'] . ";";
 $mysqli->query($sql);
+//echo($sql);
 
 header('Location: cars.php');
 ?>
