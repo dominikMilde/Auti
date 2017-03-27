94f8e4f2a86b4271791f568cb285826bc2aad23e
Dominik Milde <dominik.milde@gmail.com>
2017-03-27 12:04:52 +0200

    ispis auta

--- automobili.php
+++ automobili.php
@@ -1,8 +1,20 @@
+<?php require('connect.php'); 
+$sql = "SELECT cars.id, brands.name, cars.model, cars.color from cars, brands WHERE brands.id=cars.brand_id ORDER BY name, model;";
+$result = $mysqli->query($sql);
+?>
 <!DOCTYPE html>
     <head>
         <title> Automobili </title>
     </head>
     <body>
         <h1>Popis</h1>
+        <ul>
+        <?php
+            while($car = $result->fetch_assoc()){
+                echo('<li>' . $car['name']. ' / ' . $car['model'] . ' / ' . $car['color'] . '</li>');
+            }
+        ?>
+                
+        </ul>
     </body>
 </html>
