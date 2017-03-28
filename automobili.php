4256f2f2cc5a71ba54b5a59cc7e9e40f9ab66d5f
Dominik Milde <dominik.milde@gmail.com>
2017-03-28 19:09:36 +0200

    dropdown sa id

--- automobili.php
+++ automobili.php
@@ -10,13 +10,31 @@ $result = $mysqli->query($sql);
     <body>
 
         <h1>Popis</h1>
-
+        <form action="automobili.php" method="POST">
         <select name = 'id'>
+        <option disabled selected value> -- select an option -- </option>
             <?php
+    
             while ($brand = $result->fetch_assoc()){
                  echo('<option value = "' . $brand['id'].'">' . $brand['name'] . ' </option>');
                 }
             ?>
         </select>
+        <input type="submit" name="submit" value="Prikaži">
+        </form>
+        <p>
+            <?php
+            $sql1 = "SELECT cars.id, cars.brand_id, brands.name, cars.model, cars.color from cars, brands WHERE brands.id=cars.brand_id and brands.id = '". $_POST['id'] . "' ORDER BY name, model;";
+            $result = $mysqli->query($sql1);
+
+            //echo($sql1);
+
+            while ($car = $result->fetch_assoc()){
+                 echo($car['id'] . " / " . $car['name'] . " / " . $car['model'] . " / " . $car['color']);
+                }
+            ?>
+
+
+        </p>
     </body>
 </html>
